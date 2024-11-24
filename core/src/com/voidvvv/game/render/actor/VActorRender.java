4a1498407418d1de2f5291390cd332baa31f531b
voidvvv <1@gmail.com>
2024-11-24 15:59:38 +0800

    add stone render

--- core/src/com/voidvvv/game/render/actor/VActorRender.java
+++ core/src/com/voidvvv/game/render/actor/VActorRender.java
@@ -3,6 +3,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.voidvvv.game.base.VActor;
 
 public interface VActorRender <T extends VActor> {
+    public void init();
 
     void render(T actor, Batch batch , float parentAlpha);
 }
---
+++ core/src/com/voidvvv/game/render/actor/test/bob/StoneRender.java
@@ -0,0 +1,36 @@
+package com.voidvvv.game.render.actor.test.bob;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.voidvvv.game.ActGame;
+import com.voidvvv.game.asset.AssetConstant;
+import com.voidvvv.game.base.test.VObstacle;
+import com.voidvvv.game.render.actor.VActorRender;
+
+public class StoneRender implements VActorRender<VObstacle> {
+    Texture stone;
+    public void init() {
+        if (stone == null) {
+            stone = ActGame.gameInstance().getAssetManager().get(AssetConstant.STONE_MAN, Texture.class,false);
+            if (stone == null) {
+                ActGame.gameInstance().getAssetManager().load(AssetConstant.STONE_MAN, Texture.class);
+                ActGame.gameInstance().getAssetManager().finishLoading();
+                stone = ActGame.gameInstance().getAssetManager().get(AssetConstant.STONE_MAN, Texture.class);
+            }
+        }
+
+    }
+
+    @Override
+    public void render(VObstacle actor, Batch batch, float parentAlpha) {
+        batch.draw(stone, actor.getX(), actor.getY(),actor.getWidth(), actor.getHeight());
+        batch.end();
+        ShapeRenderer shapeRenderer = ActGame.gameInstance().getDrawManager().getShapeRenderer();
+        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+        shapeRenderer.begin();
+        shapeRenderer.rect(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
+        shapeRenderer.end();
+        batch.begin();
+    }
+}
