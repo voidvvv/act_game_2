1977a104ac8c387e491edddf40f3752b49cf045c
voidvvv <1@gmail.com>
2025-01-25 21:59:01 +0800

    add explanation

--- core/src/com/voidvvv/game/base/skill/v2/PluginSkill.java
+++ core/src/com/voidvvv/game/base/skill/v2/PluginSkill.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.utils.Pools;
 import com.voidvvv.game.plugin.Plugin;
 import com.voidvvv.game.plugin.SkillPlugin;
 
+
+/**
+ * plugin skill is for some skill that will apply plugins to character
+ */
 public abstract class PluginSkill extends Skill {
 
     public Class<? extends SkillPlugin> pluginClass;
